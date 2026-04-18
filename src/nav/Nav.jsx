aff9478ac4dea80f39a496b8bde17aea1d6d3089
MOAD Mekki <moadahmedabdesselammekki@gmail.com>
2026-04-18 16:09:51 +0100

    feat: add photo

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,9 +1,13 @@
 import './Nav.css'
+import Me from '/public/graphics/Me.JPG'
 
 export default function Nav() {
     return(
-        <div className='nav-container'>
-            <h1 className='title'>Moad Mekki</h1>
+        <div className='nav-div'>
+            <div className="nav-cont">
+                <img src={Me} alt="picture" />
+                <h1 className='title'>Moad Mekki</h1>
+            </div>
         </div>
     );
 }
