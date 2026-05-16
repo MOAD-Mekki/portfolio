3eb6a8d98dd790703787937e6e209d08f0b0d3c3
MOAD Mekki <moadahmedabdesselammekki@gmail.com>
2026-05-16 21:14:46 +0100

    feat: add animation to cards

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -1,18 +1,23 @@
 import './Education.css';
 import ENSTTIC from '/public/graphics/ensttic.jpg';
 import HCIA from '/public/graphics/HCIA.png';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ThemeContext } from '../theme/ThemeProvider';
+import AOS from "aos";
+import "aos/dist/aos.css"
 
 
 export default function Education() {
     const { isDark, toggleTheme } = useContext(ThemeContext);
+    useEffect(() => {
+        AOS.init();
+      }, []);
 
     return (
         <div className='educ-div'>
             <div className='educ-title' style={{color: isDark ? "#ffff" : "rgb(0,90,180)"}}><h2>Education & Certeficates :</h2></div>
             <div className='educ-cont'>
-                <div className='educ'>
+                <div className='educ'  data-aos="fade-right">
                     <div className='educ-img'><img src={ENSTTIC} alt="picture" /></div>
                     <div className='educ-desc'>
                         <div className='first-cont' style={{color: isDark ? "rgb(221, 218, 218)" : "rgb(50,50,70)"}}>
@@ -22,7 +27,7 @@ export default function Education() {
                     </div>
                 </div>
                 <div className='educ-cont'>
-                <div className='educ'>
+                <div className='educ' data-aos="fade-left">
                     <div className='hcia-img'><img src={HCIA} alt="picture" /></div>
                     <div className='educ-desc'>
                         <div className='second-cont' style={{color: isDark ? "rgb(221, 218, 218)" : "rgb(50,50,70)"}}>
